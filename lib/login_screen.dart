ef1d4f7be0c2efe53cc897f5e206bbaab2e5a14d
Omneya.Osaman <omneya.osaman@devexy.com>
2021-09-25 17:03:40 +0200

    initial login design

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,19 +1,88 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  LoginScreen({Key? key}) : super(key: key);
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text(
-            'Login',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 30.0, color: Colors.blue),
-          )
-        ],
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        // width: double.infinity,
+        child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Login',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 30.0, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      onFieldSubmitted: (value) {
+                        print('on Field Submitted $value');
+                      },
+                      onChanged: (value) {
+                        print('onChanged $value');
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Email',
+                          prefixIcon: Icon(Icons.email),
+                          border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: true,
+                      onFieldSubmitted: (value) {
+                        print('on Field Submitted $value');
+                      },
+                      onChanged: (value) {
+                        print('onChanged $value');
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Password',
+                          prefixIcon: Icon(Icons.lock),
+                          suffixIcon: Icon(Icons.remove_red_eye),
+                          border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: MaterialButton(
+                        onPressed: () {
+                          print(
+                              "on Login pressed ${emailController.text} ${passwordController.text}");
+                        },
+                        color: Colors.blue,
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )),
       ),
     );
   }
